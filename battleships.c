b8b1977bc8cfa98bc219205c7d34ed5558949e18
Scott <gli65@uclive.ac.nz>
2020-10-06 17:42:12 +1300

    battleships adding a function of display round term number

--- battleships.c
+++ battleships.c
@@ -3,6 +3,7 @@
     @date   30 Sep 2020
 */
 
+#include <string.h>
 #include <stdlib.h>
 #include <stdbool.h>
 #include "system.h"
@@ -16,24 +17,25 @@
 #include "battleships.h"
 
 /* Returns the vector addition of points a and b */
-tinygl_point_t vectorAdd(tinygl_point_t a, tinygl_point_t b) 
+tinygl_point_t vectorAdd(tinygl_point_t a, tinygl_point_t b)
 {
     return tinygl_point(a.x + b.x, a.y + b.y);
 }
 
 /* Returns the grid position of a Ship's offset given an offsetIndex */
-tinygl_point_t getGridPosition(Ship* ship, int offsetIndex) 
+tinygl_point_t getGridPosition(Ship* ship, int offsetIndex)
 {
     return vectorAdd(ship->pos, ship->offsets[offsetIndex]);
 }
 
 /* Checks equality of two tingygl_points A and B */
-bool isEqual(tinygl_point_t pointA, tinygl_point_t pointB) {
+bool isEqual(tinygl_point_t pointA, tinygl_point_t pointB)
+{
     return (pointA.x == pointB.x && pointA.y == pointB.y);
 }
 
 /* Checks if point on the grid is vacant */
-bool isPointVacant(tinygl_point_t point, Ship* ships, int nShips) 
+bool isPointVacant(tinygl_point_t point, Ship* ships, int nShips)
 {
     bool pointIsVacant = true;
     tinygl_point_t gridPosition;
@@ -41,9 +43,9 @@ bool isPointVacant(tinygl_point_t point, Ship* ships, int nShips)
     bool isShipActive;
     bool isShipPlaced;
     for (int i = 0; i < nShips; i++) {
-        
+
         currentShip = &ships[i];
-        
+
         for (int j = 0; j < currentShip->nOffsets; j++) {
             gridPosition = getGridPosition(currentShip, j);
             isShipActive = currentShip->isActive;
@@ -71,21 +73,21 @@ void rotateShip(Ship* ship)
 {
     tinygl_point_t newOffset;
     tinygl_point_t offsetGridPosition;
-    
+
     // Rotate each offset using 2D rotation matrix
     for (int i = 0; i < ship->nOffsets; i++) {
         newOffset = tinygl_point(-(ship->offsets[i]).y, (ship->offsets[i]).x);
-        
+
         ship->offsets[i] = newOffset;
-        
+
         offsetGridPosition = getGridPosition(ship, i);
-        
+
         if (offsetGridPosition.x < TOP_BOUND) {
             moveShip(ship, tinygl_point(1,0));
         } else if (offsetGridPosition.x > BOTTOM_BOUND) {
             moveShip(ship, tinygl_point(-1,0));
         }
-        
+
         if (offsetGridPosition.y < RIGHT_BOUND) {
             moveShip(ship, tinygl_point(0,1));
         } else if (offsetGridPosition.y > LEFT_BOUND) {
@@ -95,22 +97,22 @@ void rotateShip(Ship* ship)
 }
 
 /* Moves a Ship across the grid in a given direction if possible */
-void moveShip(Ship* ship, tinygl_point_t moveDirection) 
+void moveShip(Ship* ship, tinygl_point_t moveDirection)
 {
     bool isValidMove = true;
     tinygl_point_t gridPosition;
     tinygl_point_t newGridPosition;
-    
+
     for (int i = 0; i < ship->nOffsets; i++) {
         gridPosition = getGridPosition(ship, i);
         newGridPosition = vectorAdd(gridPosition, moveDirection);
-        
+
         if(!isWithinGrid(newGridPosition)) {
             isValidMove = false;
             break;
         }
     }
-    
+
     if (isValidMove) {
         ship->pos = vectorAdd(ship->pos, moveDirection);
     }
@@ -124,22 +126,22 @@ void drawShip(Ship* ship)
     }
 }
 
-/* Attempts to place a Ship at it's current position, returns 
+/* Attempts to place a Ship at it's current position, returns
  * whether placement was successful or not. */
 bool placeShip(Ship* ship, Ship* ships, int nShips)
 {
     bool isValidPlacement = true;
     tinygl_point_t gridPosition;
-    
+
     for (int i = 0; i < ship->nOffsets; i++) {
         gridPosition = getGridPosition(ship, i);
-        
+
         if(!isPointVacant(gridPosition, ships, nShips)) {
             isValidPlacement = false;
             break;
         }
     }
-    
+
     if (isValidPlacement) {
         ship->isActive = 0;
         ship->isPlaced = 1;
@@ -159,7 +161,8 @@ void drawBoard(Ship* ships, int nShips)
     }
 }
 
-void resetShip(Ship* ship) {
+void resetShip(Ship* ship)
+{
     ship->pos.x = DEFAULT_POS_X;
     ship->pos.y = DEFAULT_POS_Y;
     ship->isPlaced = false;
@@ -167,6 +170,29 @@ void resetShip(Ship* ship) {
     ship->isActive = false;
 }
 
+/* Game Round Term Show on display round one two three*/
+void roundTerm(int termNumber)
+{
+    //round term number start with one end with 3.
+    /* TODO: Initialise tinygl. */
+    tinygl_init (500); //PACER_RATE
+    tinygl_font_set (&font5x7_1);
+    tinygl_text_speed_set (25); //MESSAGE_RATE
+    tinygl_text_mode_set (TINYGL_TEXT_MODE_SCROLL);
+
+    /* TODO: Set the message using tinygl_text().  */
+    char* part_of_text = "Round ";
+    char t_number = (char) (termNumber + '0'); // int to char
+    char* message_text = malloc(sizeof(char)*7);//9 char we need now.
+    strncpy(message_text, part_of_text, 6);
+    message_text[6] = t_number;
+    message_text[7] = 0;
+    tinygl_text(message_text);
+
+    free(message_text);
+
+
+}
 
 int main(void)
 {
@@ -176,10 +202,10 @@ int main(void)
     led_init();
     tinygl_init(LOOP_RATE);
     pacer_init(LOOP_RATE);
-    
+
     enum game_state state = PLACE_SHIPS;
-    
-    
+
+
     Ship ships[] = {
         L_ship,
         I_ship,
@@ -188,7 +214,7 @@ int main(void)
         o_ship,
         o_ship
     };
-    
+
     Ship* currentShip = &ships[0];
     currentShip->isActive = 1;
     int nShips = 6;
@@ -196,71 +222,74 @@ int main(void)
     bool placementSuccess = false;
     int ledState = 0;
     int tick = 0;
-    
+    int termNumber = 1;
+
+
+
     while(1) {
         pacer_wait();
         navswitch_update();
         button_update();
         switch (state) {
-            case WAIT_FOR_CONNECT :
-                /* TODO: IR comms connect */
-                continue;
-                break;
-            
-            case PLACE_SHIPS :
-                /* TODO: Refactor into function */
-                tinygl_clear();
-                drawBoard(ships, nShips);
-                if (button_push_event_p(BUTTON1)) {
-                    // Place a ship
-                    placementSuccess = placeShip(currentShip, ships, nShips);
-                    if (placementSuccess) {
-                        nShipsPlaced++;
-                        currentShip++;
-                        currentShip->isActive = 1;
-                    }
+        case WAIT_FOR_CONNECT :
+            /* TODO: IR comms connect */
+            continue;
+            break;
+
+        case PLACE_SHIPS :
+            /* TODO: Refactor into function */
+            tinygl_clear();
+            drawBoard(ships, nShips);
+            if (button_push_event_p(BUTTON1)) {
+                // Place a ship
+                placementSuccess = placeShip(currentShip, ships, nShips);
+                if (placementSuccess) {
+                    nShipsPlaced++;
+                    currentShip++;
+                    currentShip->isActive = 1;
                 }
-                if (navswitch_push_event_p(NAVSWITCH_WEST)) {
-                    moveShip(currentShip, tinygl_point(-1,0));
-                }
-                if (navswitch_push_event_p(NAVSWITCH_EAST)) {
-                    moveShip(currentShip, tinygl_point(1, 0));
-                }
-                if (navswitch_push_event_p(NAVSWITCH_NORTH)) {
-                    moveShip(currentShip, tinygl_point(0, -1));
-                }
-                if (navswitch_push_event_p(NAVSWITCH_SOUTH)) {
-                    moveShip(currentShip, tinygl_point(0,1));
-                }
-                if (navswitch_push_event_p(NAVSWITCH_PUSH)) {
-                    rotateShip(currentShip);
-                }
-                
-                
-                if (nShipsPlaced == nShips) {
-                    state = GAME_RUN;
-                }
-                break;
-                
-            case GAME_RUN :
-                /* TODO: */
-                drawBoard(ships, nShips);
-                break;
-                
-            case GAME_OVER:
-                continue;
-                break;
             }
-            
+            if (navswitch_push_event_p(NAVSWITCH_WEST)) {
+                moveShip(currentShip, tinygl_point(-1,0));
+            }
+            if (navswitch_push_event_p(NAVSWITCH_EAST)) {
+                moveShip(currentShip, tinygl_point(1, 0));
+            }
+            if (navswitch_push_event_p(NAVSWITCH_NORTH)) {
+                moveShip(currentShip, tinygl_point(0, -1));
+            }
+            if (navswitch_push_event_p(NAVSWITCH_SOUTH)) {
+                moveShip(currentShip, tinygl_point(0,1));
+            }
+            if (navswitch_push_event_p(NAVSWITCH_PUSH)) {
+                rotateShip(currentShip);
+            }
+
+
+            if (nShipsPlaced == nShips) {
+                state = GAME_RUN;
+            }
+            break;
+
+        case GAME_RUN :
+            /* TODO: */
+            drawBoard(ships, nShips);
+            break;
+
+        case GAME_OVER:
+            continue;
+            break;
+        }
+
         tick++;
         if (tick > LOOP_RATE / TICK_SPEED) {
             tick = 0;
             ledState = !ledState;
         }
-        
+
         led_set(LED1, ledState);
         tinygl_update();
     }
-    
+
 }
 
