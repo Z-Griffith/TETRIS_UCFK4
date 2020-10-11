a0ac85e52613c4820f35cf3cd26dd806ad8c7de6
Scott Li <gli65@uclive.ac.nz>
2020-10-11 23:39:27 +1300

    Update ir_comms.c

--- ir_comms.c
+++ ir_comms.c
@@ -1,8 +1,8 @@
-/** @file   ir_comms.c
+/**
+    @file   ir_comms.c
     @author S. Li (gli65), S.A. Heslip (she119)
     @date   11 Oct 2020
 */
-
 #include <stdlib.h>
 #include "ir_uart.h"
 #include "ir_comms.h"
